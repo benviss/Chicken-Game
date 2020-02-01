c3fe2296dc3545c7cc8bc595c641459e3856f99c
Spencer Fisher <Fisher.spencer@gmail.com>
2020-02-01 01:15:58 -0800

    Grass and basic controls

---
+++ Chicken-Game/Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CameraController : MonoBehaviour
+{
+    Rigidbody playerBody;
+    Vector3 targetPos;
+    Vector3 offset = new Vector3(0, 17, -15);
+    float maxForce = 1;
+    float maxSpeed = 10;
+    float minSpeed = .01f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerBody = GameManager.Instance.player.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = playerBody.position + offset;
+    }
+}
---
+++ Chicken-Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    Vector3 velocity;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    public void FixedUpdate()
+    {
+        rigidBody.MovePosition(rigidBody.position + velocity * Time.fixedDeltaTime);
+    }
+}
