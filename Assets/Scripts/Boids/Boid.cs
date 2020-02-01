b3e950923cc5fc701e301cb146ef613796f98e05
benviss <ben.vissotzky@gmail.com>
2020-02-01 12:15:31 -0800

    Includes basic boid logic starting with the burds. Also includes state machine and one state(boid flock)

---
+++ Assets/Scripts/Boids/Boid.cs
@@ -0,0 +1,71 @@
+﻿//COURTESY OF SEBASTIAN LAGUE A HERO AMONG MEN
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour
+{
+    [HideInInspector]
+    public BoidSettings settings;
+
+    // State
+    [HideInInspector]
+    public Vector3 position;
+    [HideInInspector]
+    public Vector3 forward;
+    [HideInInspector]
+    public Vector3 velocity;
+
+    // To update:
+    public Vector3 acceleration;
+    [HideInInspector]
+    public Vector3 avgFlockHeading;
+    [HideInInspector]
+    public Vector3 avgAvoidanceHeading;
+    [HideInInspector]
+    public Vector3 centreOfFlockmates;
+    [HideInInspector]
+    public int numPerceivedFlockmates;
+
+    // Cached
+    public Material material;
+    public Transform cachedTransform;
+    public Transform target;
+
+    StateMachine stateMachine = new StateMachine();
+
+    void Start()
+    {
+        stateMachine.ChangeState(new BoidFlockState(this));
+    }
+
+    void Awake()
+    {
+        material = transform.GetComponentInChildren<MeshRenderer>().material;
+        cachedTransform = transform;
+    }
+
+    public void Initialize(BoidSettings settings, Transform target)
+    {
+        this.target = target;
+        this.settings = settings;
+
+        position = cachedTransform.position;
+        forward = cachedTransform.forward;
+
+        float startSpeed = (settings.minSpeed + settings.maxSpeed) / 2;
+        velocity = transform.forward * startSpeed;
+    }
+
+    public void SetColour(Color col)
+    {
+        if (material != null) {
+            material.color = col;
+        }
+    }
+
+    public void UpdateBoid()
+    {
+        stateMachine.Update();
+    }
+}
---
+++ Assets/Scripts/testscript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testscript : MonoBehaviour
+{
+    float growth = 0;
+    float growthRate = .5f;
+    float spawnTimer = .5f;
+    float age = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(Grow());
+    }
+
+
+
+    private IEnumerator Grow()
+    {
+        while (growth < 1) {
+            growth += (Time.deltaTime / growthRate);
+            //UpdateScale();
+            yield return null;
+        }
+
+        while (true) {
+            if (!IsCrowded()) {
+                TrySpawn();
+            } else {
+                age += spawnTimer;
+            }
+            yield return new WaitForSeconds(spawnTimer);
+        }
+    }
+
+
+    public bool IsCrowded()
+    {
+        return false;
+    }
+
+    public void TrySpawn()
+    {
+        Debug.Log("hmm");
+    }
+
+
+}
