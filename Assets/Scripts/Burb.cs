e10180665afc00969317e5907a4fb40537a770f1
benviss <ben.vissotzky@gmail.com>
2020-02-01 20:51:44 -0800

    the flock eats now

---
+++ Assets/Scripts/Burb.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Burb : MonoBehaviour, IEats
+{
+    Boid boid;
+    float attackDistance = .7f;
+    float damage = 1;
+    float attackCooldown = .1f;
+    float lastAttack = 0;
+    bool attacking;
+    public float grazeRange;
+    Coroutine attackCoroutine;
+    SpriteOrienter spOrient;
+
+    Vector3 gizmoPos;
+    float gizmoRad;
+
+    public float energy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boid = GetComponent<Boid>();
+        boid.switchState(new GrazeState(boid, this));
+        spOrient = GetComponent<SpriteOrienter>();
+        energy = 30;
+
+    }
+
+    void FixedUpdate()
+    {
+        energy -= Time.fixedDeltaTime;
+    }
+
+    public void TryAttack()
+    {
+        if (!attacking && (lastAttack + attackCooldown < Time.time)) {
+            attackCoroutine = StartCoroutine(AttackAnimation());
+        }
+    }
+
+    private IEnumerator AttackAnimation()
+    {
+        attacking = true;
+        bool hasAttacked = false;
+        float attackTimeStart = .2f;
+        float attackTime = attackTimeStart;
+        float percentMod = 2 / attackTime;
+        float percent = 1;
+
+        while (attackTime > 0) {
+            attackTime -= Time.deltaTime;
+
+            percent = Mathf.Abs((attackTime * percentMod) - 1);
+            float squish = Mathf.Lerp(1.5f, 1f, percent);
+            float stretch = Mathf.Lerp(.5f, 1f, percent);
+
+            transform.localScale = new Vector3(squish, stretch, squish);
+
+            if (!hasAttacked && (attackTime < attackTimeStart * .5f)) {
+                hasAttacked = true;
+                Attack();
+            }
+
+            yield return null;
+        }
+
+        transform.localScale = new Vector3(1, 1, 1);
+        attacking = false;
+        lastAttack = Time.time;
+    }
+
+    private void Attack()
+    {
+        // should be able to hit whatever - for now plants
+        int layerMask = LayerMask.GetMask("Plant");
+        Vector3 attackPos = (transform.position);
+        if (spOrient.getFacingRight()) {
+            attackPos.x += attackDistance;
+        } else {
+            attackPos.x -= attackDistance;
+        }
+
+        attackPos.y = attackDistance * .5f;
+        Collider[] hitColliders = Physics.OverlapSphere(attackPos, attackDistance, layerMask);
+        gizmoPos = attackPos;
+        gizmoRad = attackDistance;
+
+        foreach (Collider c in hitColliders) {
+            IDamagable damagableObject = c.GetComponent<IDamagable>();
+
+            if (damagableObject != null) {
+                damagableObject.TakeHit(damage, this.GetComponent<IEats>());
+            }
+        }
+    }
+
+    void startGrazing()
+    {
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(gizmoPos, gizmoRad);
+    }
+
+
+    public void GetFood(float foodAmount)
+    {
+        energy += foodAmount;
+    }
+}
--- Assets/Scripts/StateMachine/GrazeState.cs
+++ Assets/Scripts/StateMachine/GrazeState.cs
@@ -4,18 +4,49 @@ using UnityEngine;
 
 public class GrazeState : IState
 {
+    Boid owner;
+    Burb burb;
+    Transform foodTarget;
+
+    public GrazeState(Boid owner, Burb burb) { this.owner = owner; this.burb = burb; }
+
     public void Enter()
     {
-        throw new System.NotImplementedException();
     }
 
     public void Execute()
     {
-        throw new System.NotImplementedException();
+        //find food target if none already
+        if (foodTarget == null) {
+            findFood();
+        }
+
+
+        if (Vector3.Distance(owner.transform.position, foodTarget.position) < 1) {
+            //consume food target if next to it
+            burb.TryAttack();
+        } else {
+            owner.MoveBoid();
+        }
+
     }
 
     public void Exit()
     {
-        throw new System.NotImplementedException();
+        owner.switchState(new BoidFlockState(owner));
+    }
+
+    public void findFood()
+    {
+        int layerMask = 1 << 9; //Layer 9
+        var foods = Physics.OverlapSphere(owner.transform.position, 10, layerMask);
+        if (foods.Length == 0) {
+            Exit();
+            return;
+        }
+
+        Collider randomFood = foods[Random.Range(0, foods.Length - 1)];
+        foodTarget = randomFood.gameObject.transform;
+        owner.target = foodTarget;
     }
 }
